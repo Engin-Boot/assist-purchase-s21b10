03e6937349b458ce81190aaa204ea574753c501d
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-10-20 23:32:23 +0530

    Update SuggestionPaths.cs

--- AssistPurchaseCaseStudy/Repository/SuggestionPaths.cs
+++ AssistPurchaseCaseStudy/Repository/SuggestionPaths.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 
-namespace AssistPurchaseCaseStudy.Models
+namespace AssistPurchaseCaseStudy.Repository
 {
     public class SuggestionPaths
     {
-        public Dictionary<string, List<string>> quesDictionary = new Dictionary<string, List<string>>();
+        private readonly Dictionary<string, List<string>> quesDictionary = new Dictionary<string, List<string>>();
 
         private readonly string _question1 = "Features";
         private readonly List<string> _featuresMap = new List<string>() { "Touch_Screen", "Handle", "Alarm", "Battery" };
@@ -16,21 +16,21 @@ namespace AssistPurchaseCaseStudy.Models
         private readonly List<string> _alarmMap = new List<string>() { "Additional Display", "ESN", "Resp", "CO2" };
         private readonly List<string> _batteryMap = new List<string>() { "Only Spo2", "ESN", "Resp", "CO2", "Others" };
 
-        private readonly List<string> _onlySPO2Map = new List<string>() { "10-15" };
-        private readonly List<string> _ESNMap = new List<string>() { "upto 10", "10-15", "above 15" };
+        private readonly List<string> _onlySpo2Map = new List<string>() { "10-15" };
+        private readonly List<string> _esnMap = new List<string>() { "upto 10", "10-15", "above 15" };
         private readonly List<string> _respMap = new List<string>() { "upto 10", "10-15", "above 15" };
-        private readonly List<string> _CO2Map = new List<string>() { "upto 10", "10-15", "above 15" };
+        private readonly List<string> _co2Map = new List<string>() { "upto 10", "10-15", "above 15" };
         private readonly List<string> _othersMap = new List<string>() { "upto 10" };
         private readonly List<string> _addDisplayMap = new List<string>() { "above 15" };
 
-        private readonly List<string> _DisplayMap = new List<string>() { "End" };
+        private readonly List<string> _displayMap = new List<string>() { "End" };
 
-        private readonly Dictionary<string, string> layerHierarchy = new Dictionary<string, string>()
+        private readonly Dictionary<string, string> _layerHierarchy = new Dictionary<string, string>()
             {{"Features", "Services"}, {"Services", "DisplaySize"}, {"DisplaySize", "lastLayer"}};
 
-        public readonly List<string> listOfLayers = new List<string>() { "Features", "Services", "DisplaySize", "lastLayer" };
+        public readonly List<string> ListOfLayers = new List<string>() { "Features", "Services", "DisplaySize", "lastLayer" };
 
-        public Dictionary<string, List<string>> ValidLayerMembers = new Dictionary<string, List<string>>();
+        public readonly Dictionary<string, List<string>> validLayerMembers = new Dictionary<string, List<string>>();
 
 
         public SuggestionPaths()
@@ -40,28 +40,28 @@ namespace AssistPurchaseCaseStudy.Models
             quesDictionary.Add("Handle", _handleMap);
             quesDictionary.Add("Alarm", _alarmMap);
             quesDictionary.Add("Battery", _batteryMap);
-            quesDictionary.Add("Only Spo2", _onlySPO2Map);
-            quesDictionary.Add("ESN", _ESNMap);
+            quesDictionary.Add("Only Spo2", _onlySpo2Map);
+            quesDictionary.Add("ESN", _esnMap);
             quesDictionary.Add("Resp", _respMap);
-            quesDictionary.Add("CO2", _CO2Map);
+            quesDictionary.Add("CO2", _co2Map);
             quesDictionary.Add("Additional Display", _addDisplayMap);
             quesDictionary.Add("Others", _othersMap);
-            quesDictionary.Add("upto 10", _DisplayMap);
-            quesDictionary.Add("10-15", _DisplayMap);
-            quesDictionary.Add("above 15", _DisplayMap);
+            quesDictionary.Add("upto 10", _displayMap);
+            quesDictionary.Add("10-15", _displayMap);
+            quesDictionary.Add("above 15", _displayMap);
 
-            ValidLayerMembers.Add(_question1, _featuresMap);
-            ValidLayerMembers.Add("Services", new List<string>() { "Only Spo2", "Additional Display", "ESN", "Resp", "CO2", "Others" });
-            ValidLayerMembers.Add("DisplaySize", new List<string>() { "upto 10", "10-15", "above 15" });
-            ValidLayerMembers.Add("lastLayer", new List<string>() { "End" });
+            validLayerMembers.Add(_question1, _featuresMap);
+            validLayerMembers.Add("Services", new List<string>() { "Only Spo2", "Additional Display", "ESN", "Resp", "CO2", "Others" });
+            validLayerMembers.Add("DisplaySize", new List<string>() { "upto 10", "10-15", "above 15" });
+            validLayerMembers.Add("lastLayer", new List<string>() { "End" });
         }
 
 
         public string NextLayer(string previousLayer)
         {
-            if (layerHierarchy.ContainsKey(previousLayer))
+            if (_layerHierarchy.ContainsKey(previousLayer))
             {
-                return layerHierarchy[previousLayer];
+                return _layerHierarchy[previousLayer];
             }
             else
             {
