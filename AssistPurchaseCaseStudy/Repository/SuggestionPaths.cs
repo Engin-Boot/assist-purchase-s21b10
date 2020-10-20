746dbef4d568388d885ff38bee96e025e3526184
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-10-20 23:50:27 +0530

    Update SuggestionPaths.cs

--- AssistPurchaseCaseStudy/Repository/SuggestionPaths.cs
+++ AssistPurchaseCaseStudy/Repository/SuggestionPaths.cs
@@ -6,7 +6,7 @@ namespace AssistPurchaseCaseStudy.Repository
 {
     public class SuggestionPaths
     {
-        private readonly Dictionary<string, List<string>> quesDictionary = new Dictionary<string, List<string>>();
+        private readonly Dictionary<string, List<string>> _quesDictionary = new Dictionary<string, List<string>>();
 
         private readonly string _question1 = "Features";
         private readonly List<string> _featuresMap = new List<string>() { "Touch_Screen", "Handle", "Alarm", "Battery" };
@@ -30,30 +30,30 @@ namespace AssistPurchaseCaseStudy.Repository
 
         public readonly List<string> ListOfLayers = new List<string>() { "Features", "Services", "DisplaySize", "lastLayer" };
 
-        public readonly Dictionary<string, List<string>> validLayerMembers = new Dictionary<string, List<string>>();
+        public readonly Dictionary<string, List<string>> ValidLayerMembers = new Dictionary<string, List<string>>();
 
 
         public SuggestionPaths()
         {
-            quesDictionary.Add(_question1, _featuresMap);
-            quesDictionary.Add("Touch_Screen", _tsMap);
-            quesDictionary.Add("Handle", _handleMap);
-            quesDictionary.Add("Alarm", _alarmMap);
-            quesDictionary.Add("Battery", _batteryMap);
-            quesDictionary.Add("Only Spo2", _onlySpo2Map);
-            quesDictionary.Add("ESN", _esnMap);
-            quesDictionary.Add("Resp", _respMap);
-            quesDictionary.Add("CO2", _co2Map);
-            quesDictionary.Add("Additional Display", _addDisplayMap);
-            quesDictionary.Add("Others", _othersMap);
-            quesDictionary.Add("upto 10", _displayMap);
-            quesDictionary.Add("10-15", _displayMap);
-            quesDictionary.Add("above 15", _displayMap);
+            _quesDictionary.Add(_question1, _featuresMap);
+            _quesDictionary.Add("Touch_Screen", _tsMap);
+            _quesDictionary.Add("Handle", _handleMap);
+            _quesDictionary.Add("Alarm", _alarmMap);
+            _quesDictionary.Add("Battery", _batteryMap);
+            _quesDictionary.Add("Only Spo2", _onlySpo2Map);
+            _quesDictionary.Add("ESN", _esnMap);
+            _quesDictionary.Add("Resp", _respMap);
+            _quesDictionary.Add("CO2", _co2Map);
+            _quesDictionary.Add("Additional Display", _addDisplayMap);
+            _quesDictionary.Add("Others", _othersMap);
+            _quesDictionary.Add("upto 10", _displayMap);
+            _quesDictionary.Add("10-15", _displayMap);
+            _quesDictionary.Add("above 15", _displayMap);
 
-            validLayerMembers.Add(_question1, _featuresMap);
-            validLayerMembers.Add("Services", new List<string>() { "Only Spo2", "Additional Display", "ESN", "Resp", "CO2", "Others" });
-            validLayerMembers.Add("DisplaySize", new List<string>() { "upto 10", "10-15", "above 15" });
-            validLayerMembers.Add("lastLayer", new List<string>() { "End" });
+            ValidLayerMembers.Add(_question1, _featuresMap);
+            ValidLayerMembers.Add("Services", new List<string>() { "Only Spo2", "Additional Display", "ESN", "Resp", "CO2", "Others" });
+            ValidLayerMembers.Add("DisplaySize", new List<string>() { "upto 10", "10-15", "above 15" });
+            ValidLayerMembers.Add("lastLayer", new List<string>() { "End" });
         }
 
 
@@ -75,9 +75,9 @@ namespace AssistPurchaseCaseStudy.Repository
             var lst = new List<string>();
             foreach (string response in layerResponse)
             {
-                if (quesDictionary.ContainsKey(response))
+                if (ValidLayerMembers.ContainsKey(response))
                 {
-                    lst = quesDictionary[response];
+                    lst = ValidLayerMembers[response];
                 }
 
                 returnObjList = returnObjList.Union(lst).ToList();
